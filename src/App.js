9763174e146c4cb8f344b48dce5fb6f0c20f7c92
rashid-ali-nadeem <63898629+rashid-ali-nadeem@users.noreply.github.com>
2020-12-09 15:10:56 +0500

    create react app and install dependencies

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        Covid-19 Tracker      
+    </div>
+  );
+}
+
+export default App;
